c4a2c3d88849e2d172f4f2f0bb29eafccc58cac1
SunanRashidAIUB <sunanrashid@gmail.com>
2021-08-05 21:23:31 +0600

    Update CreateEmployee.js

--- final lab exam/react-test/src/components/CreateEmployee.js
+++ final lab exam/react-test/src/components/CreateEmployee.js
@@ -3,22 +3,9 @@ import React,{ Component } from "react";
 
 
 
-  const CreateUser = ({status})=>{
+  const CreateEmployee = ({status})=>{
     const {id:eid} = useParams();
-   /* constructor(props){
-        super(props)
 
-        this.state={
-            name:''
-        }
-    }
-
-    handlenameChange= (event)=>{
-        this.setState({
-            name:event.target.value
-
-        })
-    }*/
    
 
 
@@ -28,8 +15,9 @@ import React,{ Component } from "react";
             <h3>{status==='add'?'Create':'Edit'} User Page: {eid}</h3>
             <form>
                 Name: <input type='text' name='name' value=""/> <br/>
-                ID: <input type='text' name='id' value="" /><br/>
-                Dept: <input type='text' name='dpet' value="" /><br/>
+                Contact: <input type='text' name='contact' value="" /><br/>
+                Username: <input type='text' name='username' value="" /><br/>
+				Password: <input type='text' name='password' value="" /><br/>
                 <input type='submit' value={status==='add'?'Create':'Update'}/>
             </form>
         </>
